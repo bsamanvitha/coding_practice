e5c21b00090cd7d0087ab64288de896fc3ffef13
Samanvithab <s97basole@gmail.com>
2018-05-03 23:29:34 -0700

    Add isPalindrome method

--- LinkedLists/_3_Problems.java
+++ LinkedLists/_3_Problems.java
@@ -1,3 +1,4 @@
+import java.util.Stack;
 
 public class _3_Problems {
 	public static void main(String[] args) {
@@ -5,6 +6,9 @@ public class _3_Problems {
 		list.prepend(1);
 		list.prepend(2);
 		list.prepend(3);
+		list.prepend(2);
+		list.prepend(1);
+		System.out.println(list.isPalindrome());
 	}
 }
 class LinkedList3 extends LinkedList {
@@ -18,5 +22,26 @@ class LinkedList3 extends LinkedList {
 	 */
 	public void reverseLL() {
 		
+	}
+	
+	public boolean isPalindrome() {
+		// push to stack
+		Stack<Node> s = new Stack<Node>();
+		Node current = first;
+		while (current != null) {
+			s.push(current);
+			current = current.next;
+		}
+		
+		current = first;
+		while (!s.isEmpty()) {
+			Node toCompare = s.pop();
+			if (toCompare.data != current.data) {
+				System.out.println(current + " " + toCompare );
+				return false;
+			}
+			current = current.next;
+		}
+		return true;
 	}
 }
