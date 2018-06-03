2a1e9eba5ea6456c7f9b82e82404b6064cc4b7dd
Samanvithab <s97basole@gmail.com>
2018-06-03 16:21:30 -0700

    Add intersection of linkedlists

--- LinkedLists/_3_Problems.java
+++ LinkedLists/_3_Problems.java
@@ -3,17 +3,31 @@ import java.util.Stack;
 public class _3_Problems {
 	public static void main(String[] args) {
 		LinkedList3 list = new LinkedList3();
+		LinkedList3 list2 = new LinkedList3();
 		list.prepend(1);
-		list.prepend(1);
-		list.prepend(1);
-		list.prepend(2);
 		list.prepend(2);
 		list.prepend(3);
+		list.prepend(4);
+		list.prepend(5);
+		list.prepend(6);
+		list2.append(9);
+		list2.append(8);
+		list2.append(7);
+		list2.append(3);
+		list2.append(2);
+		list2.append(1);
+		
+		/*
 		System.out.println(list.isPalindrome());
 		list.printList();
 		list.removeDuplicatesFromSorted();
 		System.out.println("After deleting duplicates");
 		list.printList();
+		*/
+		list.printList();
+		System.out.println("------------------");
+		list2.printList();
+		System.out.print("Intersection node is: " + list.intersectionOfTwoLists(list2));
 	}
 }
 class LinkedList3 extends LinkedList {
@@ -60,5 +74,17 @@ class LinkedList3 extends LinkedList {
 				current = current.next;
 			}
 		}
+	}
+	
+	public Node intersectionOfTwoLists(LinkedList3 list2) {
+		
+		for (Node current = first; current.next != null; current = current.next) {
+			for (Node curr = list2.first; curr.next != null; curr = curr.next) {
+				if (current.data == curr.data) { //should compare address not contents
+					return current;
+				}
+			}
+		}
+		return null;
 	}
 }
