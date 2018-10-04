f6374a7f5033dfc4179868827ac1b12bb5c67c64
samanvithab <s97basole@gmail.com>
2018-10-04 12:56:22 -0700

    Add insert method

--- Trees/src/BinarySearchTree.java
+++ Trees/src/BinarySearchTree.java
@@ -10,12 +10,40 @@ public class BinarySearchTree {
 		if (current == null || current.data == d) {
 			return current;
 		}
-		if (d > current.data) {
+		if (d < current.data) {
 			return find(current.left, d);
 		}
-		return find(current.right, d)
+		if (d > current.data) {
+			return find(current.right, d);
+		}
+		return null;
 	}
 	
+	public void insert(int d) {
+		insert(root, d);
+	}
+
+	private void insert(TreeNode current, int d) {
+		if (current == null) {
+			current = new TreeNode(d);
+		}
+		if (d > current.data) {
+			if (current.right == null) {
+				current.right = new TreeNode(d);
+			}
+			else {
+				insert(current.right, d);
+			}
+		}
+		else {
+			if (current.left == null) {
+				current.left = new TreeNode(d);
+			}
+			else {
+				insert(current.left, d);
+			}
+		}
+	}
 	
 }
 
